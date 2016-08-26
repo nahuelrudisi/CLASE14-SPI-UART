62de38a91899608a1777918d640795aa3ad15e7f
Martin Marcucci <rmarku@gmail.com>
2016-08-26 10:06:14 -0300

    se agrego queue. Ver si anda

---
+++ src/queue.c
@@ -0,0 +1,40 @@
+/*
+ * queue.c
+ *
+ *  Created on: 26 de ago. de 2016
+ *      Author: LCSR-AF
+ */
+#include "queue.h"
+
+inline int incrementar(int n) {
+	return ++n%16;
+}
+
+void queue_init(Queue *q) {
+	q->fondo = 0;
+	q->frente = 0;
+	q->tamanio = 0;
+}
+
+int queue_encolar(Queue *q, char d) {
+	int err =1;
+	if (q->tamanio < 16) {
+		q->datos[q->fondo] = d;
+		q->fondo = incrementar(q->fondo);
+		q->tamanio++;
+		err=0;
+	}
+	return err;
+}
+
+char queue_desencolar(Queue *q, int *err) {
+	char tmp;
+	*err = 1;
+	if (q->tamanio > 0) {
+		tmp = q->datos[q->frente];
+		q->frente = incrementar(q->frente);
+		*err = 0;
+		q->tamanio--;
+	}
+	return tmp;
+}
---
+++ src/queue.h
@@ -0,0 +1,25 @@
+/*
+ * queue.h
+ *
+ *  Created on: 26 de ago. de 2016
+ *      Author: LCSR-AF
+ */
+
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+
+typedef struct  {
+	int frente;
+	int fondo;
+	int tamanio;
+	char datos[16];
+} Queue;
+
+
+void queue_init(Queue *q);
+int queue_encolar(Queue *q, char d);
+char queue_desencolar(Queue *q, int *err);
+
+
+#endif /* QUEUE_H_ */
